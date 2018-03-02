a5ee811c0bc200f9bbd0559a9ba71d5f05b392b2
Yiğitcan UÇUM <yigitcan@hotmail.com.tr>
2018-03-03 01:13:00 +0300

    initial commit building typescript as node module.

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export function PdfParser(){ return 'PDF'; }
+export function HtmlParser(){ return 'Html'; }
+export function TextParser(){ return 'Text'; }
+
+export default HtmlParser;
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  output: {
+    filename: '[name].js',
+    library: 'soyagaci-parser',
+    libraryTarget: 'commonjs2',
+    path: path.join(__dirname, 'dist'), // where to place webpack files
+  },
+  entry: {
+    main: './src/index.ts'
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [{
+      test: /\.ts$/,
+      use: [
+        {
+          loader: 'ts-loader',
+        }
+      ]
+    }],
+  },
+  target: 'web'
+};
