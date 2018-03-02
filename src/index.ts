1dfb57ac85208d98d9bc109edddf42a7356afd01
Yiğitcan UÇUM <yigitcan@hotmail.com.tr>
2018-03-03 01:40:59 +0300

    make project run as a node script.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+//import pdf1 from 'pdfjs';
+declare function require(name:string);
+const pdf1 = require('pdfjs-dist');
+
+console.log(pdf1);
+
 export function PdfParser(){ return 'PDF'; }
 export function HtmlParser(){ return 'Html'; }
 export function TextParser(){ return 'Text'; }
--- webpack.config.js
+++ webpack.config.js
@@ -23,5 +23,5 @@ module.exports = {
       ]
     }],
   },
-  target: 'web'
+  target: 'node'
 };
