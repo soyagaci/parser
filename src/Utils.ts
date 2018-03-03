d1f89a3072a0bcdd4d0e5bee1fee93a7c21f5c54
Yiğitcan UÇUM <yigitcan@hotmail.com.tr>
2018-03-04 02:56:22 +0300

    fix parse turkish date as utc.

--- src/Utils.ts
+++ src/Utils.ts
@@ -1,4 +1,4 @@
 export function parseTurkishDate(str: string) : Date{
     const split = str.split("/");
-    return new Date(parseInt(split[2], 10), parseInt(split[1], 10) - 1, parseInt(split[0], 10));
+    return new Date(Date.UTC(parseInt(split[2], 10), parseInt(split[1], 10) - 1, parseInt(split[0], 10)));
 }
