4cb37526e6d709be87a2ddd07c87f5f1869913a5
Yiğitcan UÇUM <yigitcan@hotmail.com.tr>
2018-03-03 23:49:51 +0300

    fix html parser test spec node require scopes.

--- __tests__/HTMLParser.spec.ts
+++ __tests__/HTMLParser.spec.ts
@@ -1,9 +1,9 @@
-const fs = require('fs');
-const path = require('path');
-const HTMLParser = require('../dist/html').default;
-const soyHTML = fs.readFileSync(path.join(__dirname, './data/soy.html'));
-
 describe('HTMLParser Spec', () => {
+    const fs = require('fs');
+    const path = require('path');
+    const HTMLParser = require('../dist/html').default;
+    const soyHTML = fs.readFileSync(path.join(__dirname, './data/soy.html'));
+
     it('should return an empty array', async () => {
         const result = await HTMLParser(soyHTML.toString());
 
