01667bd03067eb9965e2a77860384e7bb534755a
Oğuzcan Yavuz <oguzcanyavuz@hotmail.com>
2018-03-04 06:52:39 +0300

    TextParser is completed. New test file created and added to tests.

--- src/TextParser.ts
+++ src/TextParser.ts
@@ -1,9 +1,12 @@
-function createResults(buffer: Buffer): object {
+import {HeaderColumn, HeaderColumnIndexPair, parseRecords} from "./RecordParse";
+
+function parseText(buffer: Buffer): object {
     const str: string = buffer.toString();
     const pattern: RegExp = /\d+\t(E|K)\t[ABK]/g;
-    let results: object = {"records": [], "errors": []};
+    let results: string[][] = [];
     let indexes: number[] = [];
     let match: any;
+    let headerColumn = HeaderColumn;
 
     // collect the indexes of matches
     while((match = pattern.exec(str)) != null) {
@@ -13,22 +16,31 @@ function createResults(buffer: Buffer): object {
     let endIndex = str.indexOf("AÇIKLAMALAR");
     indexes.push(endIndex);
 
-    // loop through indexes array and append results to results object
+    // loop through "indexes" array and push results to the "results" array
     for(let i: number = 0; i < indexes.length - 1; i++) {
         let record: string[] = str.slice(indexes[i], indexes[i + 1]).split("\t");
-        let relation: string[] = [];
-        record[2].split("").forEach(function(c) {
-            if(c >= "A" && c <= "Z")
-                relation.push(c);
-        });
-        results["records"].push({"record": record, "relation": relation.join("")});
+        results.push(record);
     }
-    console.log(results["records"][0]);
-    return results;
+
+    let headerColumnIndexPairs: HeaderColumnIndexPair[] = [
+        [headerColumn.Order, 0],
+        [headerColumn.Gender, 1],
+        [headerColumn.Relation, 2],
+        [headerColumn.Name, 3],
+        [headerColumn.LastName, 4],
+        [headerColumn.FathersName, 5],
+        [headerColumn.MothersName, 6],
+        [headerColumn.MarriageStatus, 10],
+        [headerColumn.BirthAddress, 8],
+        [headerColumn.BirthPlaceAndDate, 7],
+        [headerColumn.DeathStatus, 11],
+        [headerColumn.CitHaneSiraNo, 9]
+    ];
+    return parseRecords(results, headerColumnIndexPairs);
 }
 
 
 export default function TextParser(buffer: Buffer): any[] {
-    createResults(buffer);
+    parseText(buffer);
     return [];
 };
